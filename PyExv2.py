175a21341106a77af7e7ccdccc3d77a6a9fe1cf0
Tarzhan <jacktasarkan@outlook.com>
2016-10-23 16:48:29 +0100

    Updated PyExv2.py
    Committed updated PyEx.py file

---
+++ PyExv2.py
@@ -0,0 +1,22 @@
+num = 4
+num*=2
+num1=num+2
+num1+=3
+print(num1)
+
+print(2 / 3)
+print(2 // 3)
+
+x = 19.93
+y = 20.00
+z = y - x
+print(z)
+
+int = x = 2
+Print (x, squared is, x * x)
+xcubed = x *** 3
+
+X = 2
+Y = 4
+print(“The product of “, x, “and”, y, “is”, x + y)
+print(“The root of their difference is “, sqrt(x – y))
