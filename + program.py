e951a35580bba12a9816236c4d0adbd774170e02
Tarzhan <jacktasarkan@outlook.com>
2016-10-23 02:09:18 +0100

    +Program
    Adding Addition Program

---
+++ + program.py
@@ -0,0 +1,10 @@
+# Basic Python addition program.
+
+number1=float(input("please enter a number"))    #The program asks the user for a number twice.
+
+number2=float(input("please enter another number"))
+
+sum = float(number1) + float(number2)
+
+print(sum)  #(print("the sum of {0} and {1} is {2}".format (number1, number2,sum))
+ 
