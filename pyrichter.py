0e687e93204d1b664106efd18d71c26078b99ac9
Tarzhan <jacktasarkan@outlook.com>
2016-10-24 15:36:18 +0100

    Adding richterprogram
    Completed practical session script

---
+++ pyrichter.py
@@ -0,0 +1,12 @@
+richter=int(input("Please enter the current richter"))
+
+if richter >= 8.0:
+    print("Most structures fall")
+elif richter >= 7.0:
+    print ("Many buildings destroyed")
+elif richter >= 6.0:
+    print ("Many buildings considerably damaged, some collapse.")
+elif richter >= 4.5:
+    print ("Damage to poorly constructed buildings.")
+elif richter >= 4.0:
+    print = ("Minimal damage sustained.")
